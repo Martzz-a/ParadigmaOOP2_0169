552100bea714490d08e8cd4c3ec2950d88371ace
Martzz-a <maritzafidelyah@gmail.com>
2026-05-18 14:59:17 +0700

    Menambahkan Class Pelajar

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -15,3 +15,10 @@ public:
         cout << "pekerja dibuat\n" << endl;
     }
 };
+
+class pelajar : virtual public orang {
+public:
+    pelajar(int pUmur) : orang(pUmur) {
+        cout << "pelajar dibuat\n" << endl;
+    }
+};
